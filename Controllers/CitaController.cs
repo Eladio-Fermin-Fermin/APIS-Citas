47a8d4911e79051f920da4a58e901ca87f5244e7
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-10-01 13:50:06 -0400

    Add project files.

---
+++ Controllers/CitaController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Cita.Models;
+using Cita.Services;
+
+namespace Cita.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitaController : ControllerBase
+    {
+        public CitaController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Citas>> GetAll() => CitaServices.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Citas> Get(int id)
+        {
+            var cita = CitaServices.Get(id);
+
+            if (cita == null)
+                return NotFound();
+
+            return cita;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Citas cita)
+        {
+            CitaServices.Add(cita);
+            return CreatedAtAction(nameof(Create), new { id = cita.CitaId }, cita);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Citas cita)
+        {
+            if (id != cita.CitaId)
+                return BadRequest();
+
+            var existingCita = CitaServices.Get(id);
+            if (existingCita is null)
+                return NotFound();
+
+            CitaServices.Update(cita);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = CitaServices.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+
+            CitaServices.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Services/CitaServices.cs
@@ -0,0 +1,42 @@
+﻿using Cita.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Cita.Controllers;
+
+
+namespace Cita.Services
+{
+    public static class CitaServices
+    {
+        static List<Citas> citas { get; set; }
+        static int nextId = 3;
+
+        public static List<Citas> GetAll() => citas;
+
+        public static Citas Get(int id) => citas.FirstOrDefault(p => p.CitaId == id);
+
+        public static void Add(Citas cita)
+        {
+            cita.CitaId = nextId++;
+            citas.Add(cita);
+        }
+
+        public static void Delete(int id)
+        {
+            var cita = Get(id);
+            if (cita is null)
+                return;
+
+            citas.Remove(cita);
+        }
+
+        public static void Update(Citas cita)
+        {
+            var index = citas.FindIndex(p => p.CitaId == cita.CitaId);
+            if (index == -1)
+                return;
+
+            citas[index] = cita;
+        }
+    }
+}
